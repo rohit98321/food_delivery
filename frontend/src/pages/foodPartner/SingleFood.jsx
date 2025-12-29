0988780dbd2f61fb97e851e25e691491fc2037e8
Rohit Gupta <rohitgupta.dev01@gmail.com>
2025-12-29 21:53:12 +0530

    food update api done

--- frontend/src/pages/foodPartner/SingleFood.jsx
+++ frontend/src/pages/foodPartner/SingleFood.jsx
@@ -1,28 +1,146 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
-import { asyncGetSingleFood } from '../../reduxToolKit/Actions/food.action'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { asyncGetSingleFood, asyncUpdateFood } from "../../reduxToolKit/Actions/food.action";
+import { useForm } from "react-hook-form";
 
 const SingleFood = () => {
+  const { id } = useParams();
 
-    const {id}=useParams()
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(asyncGetSingleFood(id));
+  }, [dispatch, id]);
+
+  const food = useSelector((state) => state.food.singleFood);
+  console.log("single food data", food);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      title: food?.title,
+      description: food?.description,
+      price: food?.price,
+      currency: food?.currency,
+    },
+  });
+
+  const foodUpdateHandler = (updatedData) => {
+      console.log("foodUpdateHandler clicked");
+    dispatch(asyncUpdateFood(updatedData,id))
     
-    const dispatch=useDispatch()
-
-    useEffect(() => {
-      
-      dispatch(asyncGetSingleFood(id));
-   
-    }, [dispatch,id])
-
-
-    const food =useSelector((state)=>state.food.singleFood)
-    console.log("single food data",food);
-    
+  };
 
   return (
-    <div>{food.title}</div>
-  )
-}
+    <div className="flex gap-4 flex-col p-4 ">
+      <div className="flex gap-4">
+        <div className="w-[500px] h-[500px]">
+          <img
+            className="w-[500px] h-[500px] object-cover"
+            src={food.poster}
+            alt="poster"
+          />
+        </div>
 
-export default SingleFood
+        <div>
+          <h2 className="text-4xl">{food.description}</h2>
+          <h2 className="text-2xl">
+            <span>Title : </span>
+            {food.title}
+          </h2>
+          <h2 className="text-2xl">
+            <span>price : </span>
+            {food.price}
+          </h2>
+        </div>
+      </div>
+
+      <div className="w-1/2 h-[600px] bg-[#57595B]  border p-3">
+        <form
+          onSubmit={handleSubmit(foodUpdateHandler)}
+          className="space-y-4 w-[50%]"
+        >
+          {/* Title */}
+          <div>
+            <label className="block font-medium text-gray-700">Title</label>
+            <input
+              type="text"
+              {...register("title", { required: true })}
+              className="w-full border rounded-lg p-2 outline-none focus:ring-2 focus:ring-blue-400"
+              placeholder="Enter food title"
+            />
+            {errors.title && (
+              <p className="text-red-500 text-sm mt-1">Title is required</p>
+            )}
+          </div>
+
+          {/* Description */}
+          <div>
+            <label className="block font-medium text-gray-700">
+              Description
+            </label>
+            <textarea
+              {...register("description", { required: true })}
+              className="w-full border rounded-lg p-2 outline-none focus:ring-2 focus:ring-blue-400"
+              placeholder="Enter food description"
+            ></textarea>
+            {errors.description && (
+              <p className="text-red-500 text-sm mt-1">
+                Description is required
+              </p>
+            )}
+          </div>
+
+          {/* Price */}
+          <div className="grid grid-cols-2 gap-3">
+            <div>
+              <label className="block font-medium text-gray-700">Amount</label>
+              <input
+                type="Number"
+                {...register("price", { required: true })}
+                className="w-full border rounded-lg p-2 outline-none focus:ring-2 focus:ring-blue-400"
+                placeholder="₹ Price"
+              />
+              {errors.price && (
+                <p className="text-red-500 text-sm mt-1">Amount required</p>
+              )}
+            </div>
+
+            <div>
+              <label className="block font-medium text-gray-700">
+                Currency
+              </label>
+              <select
+                {...register("currency", { required: true })}
+                className="w-full border rounded-lg p-2 outline-none focus:ring-2 focus:ring-blue-400"
+              >
+                <option value="">Select</option>
+                <option value="INR">INR ₹</option>
+                <option value="USD">USD $</option>
+                <option value="EUR">EUR €</option>
+              </select>
+              {errors.currency && (
+                <p className="text-red-500 text-sm mt-1">Currency required</p>
+              )}
+            </div>
+          </div>
+
+          {/* Submit */}
+          <button
+            type="submit"
+            className="bg-blue-800 m-auto outline-0 border p-2 rounded text-white"
+          >
+            Update Food
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SingleFood;
--- frontend/src/reduxToolKit/Actions/food.action.jsx
+++ frontend/src/reduxToolKit/Actions/food.action.jsx
@@ -1,58 +1,64 @@
-import { toast } from "react-toastify"
-import axios from "../../Api/config"
+import { toast } from "react-toastify";
+import axios from "../../Api/config";
 import { loadSingleFood, loadVideos } from "../Slices/food.slice";
 import { loadPartnerfoods } from "../Slices/food.slice";
 
-
 export const asyncGetVideos = (details) => async (dispatch, getState) => {
-    
-    try {
-        const res=await axios.get("/food/getall",{withCredentials:true})
-        console.log("videoAction",res.data);
-        dispatch(loadVideos(res.data.fooditems))
-    } catch (error) {
-        
-    }
+  try {
+    const res = await axios.get("/food/getall", { withCredentials: true });
+    console.log("videoAction", res.data);
+    dispatch(loadVideos(res.data.fooditems));
+  } catch (error) {}
+};
 
-  };
-
-
-  export const asyncGetFoodById = (id) => async (dispatch, getState) => {
-    
-    try {
-        const res=await axios.get(`/food/getall/${id}`,{withCredentials:true})
-        console.log("videoAction",res.data);
-        dispatch(loadPartnerfoods(res.data.fooditems))
-    } catch (error) {
-        toast.error(error.response?.data?.message)
-    }
-
-  };
-
-  export const asyncGetPatnerAllFood = () => async (dispatch, getState) => {
-    
-    try {
-        const res=await axios.get("/food/getbyId",{withCredentials:true})
-        console.log("parntnerfoods",res.data);
-        dispatch(loadPartnerfoods(res.data.fooditems))
-       
-    } catch (error) {
-        toast.error(error.response?.data?.message)
-    }
-
-  };
-
-
-  export const asyncGetSingleFood=(id)=>async(dispatch,getState)=>{
-        console.log("asyncGetSingleFood action invoked",id);
-    try {
-        const res=await axios.get("/food/singleProduct/"+id,{withCredentials:true})
-
-        console.log("response from single food action",res);
-        dispatch(loadSingleFood(res.data.product))
-        
-    } catch (error) {
-        toast.error(error.response?.data?.message)
-        console.log(error);
-    }
+export const asyncGetFoodById = (id) => async (dispatch, getState) => {
+  try {
+    const res = await axios.get(`/food/getall/${id}`, {
+      withCredentials: true,
+    });
+    console.log("videoAction", res.data);
+    dispatch(loadPartnerfoods(res.data.fooditems));
+  } catch (error) {
+    toast.error(error.response?.data?.message);
   }
+};
+
+export const asyncGetPatnerAllFood = () => async (dispatch, getState) => {
+  try {
+    const res = await axios.get("/food/getbyId", { withCredentials: true });
+    console.log("parntnerfoods", res.data);
+    dispatch(loadPartnerfoods(res.data.fooditems));
+  } catch (error) {
+    toast.error(error.response?.data?.message);
+  }
+};
+
+export const asyncGetSingleFood = (id) => async (dispatch, getState) => {
+  console.log("asyncGetSingleFood action invoked", id);
+  try {
+    const res = await axios.get("/food/singleProduct/" + id, {
+      withCredentials: true,
+    });
+
+    console.log("response from single food action", res);
+    dispatch(loadSingleFood(res.data.product));
+  } catch (error) {
+    toast.error(error.response?.data?.message);
+    console.log(error);
+  }
+};
+
+export const asyncUpdateFood =
+  (updatedData,id) => async (dispatch, getState) => {
+
+    try {
+        const res=await axios.patch("/food/update"+id,updatedData,{withCredentials:true})
+        dispatch(loadSingleFood(res.data.product));
+        console.log("asyncUpdateFood",res);
+
+        
+    } catch (error) {
+        toast.error(error.response?.data?.message);
+    }
+
+  };
